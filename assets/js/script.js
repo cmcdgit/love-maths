dd71c32d780a97053eb487730513178b47878088
Ciaran Mc Devitt <cmcdgit@gmail.com>
2024-08-20 11:36:07 +0000

    Adding Enter button press functionality, tidy up

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,21 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    /**
+     * Working solution for Enter button submit
+     */
+    // document.addEventListener('keydown', (event) => {
+    //     if (event.key === 'Enter') {
+    //       event.preventDefault(); // prevent default behavior
+    //       checkAnswer(); // submit the form
+    //     }
+    //   });
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if(event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
 })
 
@@ -23,6 +38,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -101,28 +120,24 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    // document.getElementById('answer-box').textContent = "";
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    // document.getElementById('answer-box').textContent = "";
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
-    // document.getElementById('answer-box').textContent = "";
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    // document.getElementById('answer-box').textContent = "";
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
